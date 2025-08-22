167380722244857b429e12b44675f2fe27d1ca25
songmoro <wlqgkr@naver.com>
2025-08-22 15:38:22 +0900

    Initial Commit

---
+++ Tamagochi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tamagochi
+//
+//  Created by 송재훈 on 8/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
