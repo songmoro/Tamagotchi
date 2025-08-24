b506f4d811ef8cb00dbd0d9d125cf19931c5738f
songmoro <wlqgkr@naver.com>
2025-08-24 18:01:50 +0900

    데이터 초기화

--- Tamagochi/Feature/Settings/SettingsViewController.swift
+++ Tamagochi/Feature/Settings/SettingsViewController.swift
@@ -23,11 +23,14 @@ struct Settings {
 }
 
 final class SettingsViewModel: ViewModel {
+    private let disposeBag = DisposeBag()
+    
     struct Input {
-        
+        let row: Observable<Int>
     }
     struct Output {
         let settings: Driver<[Settings]>
+        let reset: Driver<Void>
     }
     
     let settings: [Settings] = [
@@ -37,9 +40,28 @@ final class SettingsViewModel: ViewModel {
     ]
     
     func transform(input: Input) -> Output {
+        let rowAction = PublishRelay<Int>()
+        let reset = PublishRelay<Void>()
         
+        input.row
+            .bind(to: rowAction)
+            .disposed(by: disposeBag)
         
-        return .init(settings: Observable.just(settings).asDriver(onErrorJustReturn: []))
+        rowAction
+            .bind {
+                if $0 == 0 {
+                    
+                }
+                else if $0 == 1 {
+                    
+                }
+                else if $0 == 2 {
+                    reset.accept(())
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        return .init(settings: Observable.just(settings).asDriver(onErrorJustReturn: []), reset: reset.asDriver(onErrorJustReturn: ()))
     }
 }
 
@@ -53,7 +75,11 @@ final class SettingsViewController: TamagochiViewController<SettingsViewModel> {
     }
     
     private func bind() {
-        let output = viewModel.transform(input: .init())
+        let output = viewModel.transform(
+            input: .init(
+                row: tableView.rx.itemSelected.map(\.row).asObservable()
+            )
+        )
         
         output.settings
             .drive(tableView.rx.items(cellIdentifier: "Cell", cellType: UITableViewCell.self)) {
@@ -66,6 +92,21 @@ final class SettingsViewController: TamagochiViewController<SettingsViewModel> {
                 $2.tintColor = .tint
                 $2.backgroundColor = .clear
                 $2.accessoryType = .disclosureIndicator
+            }
+            .disposed(by: disposeBag)
+        
+        output.reset
+            .drive(with: self) { owner, _ in
+                let alert = UIAlertController(title: "데이터 초기화", message: "정말 다시 처음부터 시작하실 건가용?", preferredStyle: .alert)
+                alert.addAction(.init(title: "아냐!", style: .cancel))
+                alert.addAction(.init(title: "웅", style: .destructive, handler: { _ in
+                    UserDefaults.standard.set(nil, forKey: "tamagochi")
+                    
+                    let vc = ChoiceViewController(viewModel: .init())
+                    (owner.view.window?.rootViewController as? UINavigationController)?.viewControllers = [vc]
+                }))
+                
+                owner.present(alert, animated: true)
             }
             .disposed(by: disposeBag)
     }
