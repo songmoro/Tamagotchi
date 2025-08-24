537e9ff9df967fb512c5b5edaec233f60143b72a
songmoro <wlqgkr@naver.com>
2025-08-24 22:00:03 +0900

    바버튼아이템틴트컬러

--- Tamagochi/AppDelegate.swift
+++ Tamagochi/AppDelegate.swift
@@ -12,6 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         UIButton.appearance().tintColor = .tint
         UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.tint]
+        UIBarButtonItem.appearance().tintColor = .tint
         return true
     }
     
