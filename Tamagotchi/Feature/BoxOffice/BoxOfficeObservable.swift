623fce3f5f03574d709e5ee3915393743bc0b9c4
songmoro <wlqgkr@naver.com>
2025-08-25 19:59:44 +0900

    영화 검색 에러 처리

--- Tamagotchi/Feature/BoxOffice/BoxOfficeObservable.swift
+++ Tamagotchi/Feature/BoxOffice/BoxOfficeObservable.swift
@@ -11,6 +11,14 @@ import RxSwift
 import RxCocoa
 
 final class BoxOfficeObservable {
+    enum ErrorReason: LocalizedError {
+        case invalidDate
+        
+        var errorDescription: String? {
+            "날짜 입력이 잘못 되었습니다.\n올바른 예: 20250825"
+        }
+    }
+    
     static let dateFormatter: DateFormatter = {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyyMMdd"
@@ -19,9 +27,10 @@ final class BoxOfficeObservable {
     }()
     
     private init() { }
-    static func movie(_ date: String) -> Observable<BoxOfficeResponse> {
+    static func movie(_ date: String) -> Observable<Result<BoxOfficeResponse, Error>> {
         return Observable.create { observer in
             guard dateFormatter.date(from: date) != nil else {
+                observer.onNext(.failure(ErrorReason.invalidDate))
                 return Disposables.create()
             }
             
@@ -33,10 +42,10 @@ final class BoxOfficeObservable {
                 .responseDecodable(of: BoxOfficeResponse.self) { response in
                     switch response.result {
                     case .success(let boxOfficeResponse):
-                        observer.onNext(boxOfficeResponse)
+                        observer.onNext(.success(boxOfficeResponse))
                         observer.onCompleted()
-                    default:
-                        break
+                    case .failure(let error):
+                        observer.onNext(.failure(error))
                     }
                 }
             
--- Tamagotchi/Feature/BoxOffice/BoxOfiiceViewModel.swift
+++ Tamagotchi/Feature/BoxOffice/BoxOfiiceViewModel.swift
@@ -23,6 +23,8 @@ final class BoxOfiiceViewModel: ViewModel {
     func transform(_ input: Input) -> Output {
         let date = PublishRelay<String>()
         let movies = PublishRelay<String>()
+        let response = PublishRelay<BoxOfficeResponse>()
+        let errorRelay = PublishRelay<Error>()
         
         input.click
             .withLatestFrom(input.text)
@@ -30,10 +32,28 @@ final class BoxOfiiceViewModel: ViewModel {
             .disposed(by: disposeBag)
         
         date
+            .do(onNext: { _ in movies.accept("영화를 불러오는 중 입니다...") })
             .flatMap(BoxOfficeObservable.movie)
+            .debug()
+            .bind {
+                switch $0 {
+                case .success(let result):
+                    response.accept(result)
+                case .failure(let error):
+                    errorRelay.accept(error)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        response
             .map(\.boxOfficeResult.dailyBoxOfficeList)
             .map { $0.map(\.movieNm) }
             .map { $0.joined(separator: "\n") }
+            .bind(to: movies)
+            .disposed(by: disposeBag)
+        
+        errorRelay
+            .map { ($0 as? LocalizedError)?.errorDescription ?? "잘못된 요청입니다." }
             .bind(to: movies)
             .disposed(by: disposeBag)
         
