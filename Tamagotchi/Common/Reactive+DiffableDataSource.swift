0fd92124fb6ca386a451960949e1c992647ba56d
songmoro <wlqgkr@naver.com>
2025-09-01 23:46:28 +0900

    rx+diffable 개선

--- Tamagotchi/Common/Reactive+DiffableDataSource.swift
+++ Tamagotchi/Common/Reactive+DiffableDataSource.swift
@@ -10,64 +10,40 @@ import RxSwift
 import RxCocoa
 
 extension Reactive where Base: UITableView {
-    func sectionSelected<SectionIdentifierType, ItemIdentifierType>(_ dataSource: UITableViewDiffableDataSource<SectionIdentifierType, ItemIdentifierType>) -> ControlEvent<SectionIdentifierType> where SectionIdentifierType: Hashable, SectionIdentifierType: Sendable, ItemIdentifierType: Hashable, ItemIdentifierType: Sendable {
-        let source: Observable<SectionIdentifierType> = self.itemSelected.flatMap { [weak view = self.base as UITableView] indexPath -> Observable<SectionIdentifierType> in
-            guard view != nil else {
-                return Observable.empty()
+    func sectionSelected<SectionIdentifierType, ItemIdentifierType>(_ dataSource: UITableViewDiffableDataSource<SectionIdentifierType, ItemIdentifierType>) -> ControlEvent<SectionIdentifierType> where SectionIdentifierType: Hashable, ItemIdentifierType: Hashable {
+        let source: Observable<SectionIdentifierType> = self.itemSelected
+            .compactMap { [weak dataSource] indexPath in
+                dataSource?.sectionIdentifier(for: indexPath.section)
             }
-            guard let section = dataSource.sectionIdentifier(for: indexPath.section) else {
-                return Observable.empty()
-            }
-            
-            return Observable.just(section)
-        }
         
         return ControlEvent(events: source)
     }
     
-    func itemSelected<SectionIdentifierType, ItemIdentifierType>(_ dataSource: UITableViewDiffableDataSource<SectionIdentifierType, ItemIdentifierType>) -> ControlEvent<ItemIdentifierType> where SectionIdentifierType: Hashable, SectionIdentifierType: Sendable, ItemIdentifierType: Hashable, ItemIdentifierType: Sendable {
-        let source: Observable<ItemIdentifierType> = self.itemSelected.flatMap { [weak view = self.base as UITableView] indexPath -> Observable<ItemIdentifierType> in
-            guard view != nil else {
-                return Observable.empty()
+    func itemSelected<SectionIdentifierType, ItemIdentifierType>(_ dataSource: UITableViewDiffableDataSource<SectionIdentifierType, ItemIdentifierType>) -> ControlEvent<ItemIdentifierType> where SectionIdentifierType: Hashable, ItemIdentifierType: Hashable {
+        let source: Observable<ItemIdentifierType> = self.itemSelected
+            .compactMap { [weak dataSource] indexPath in
+                dataSource?.itemIdentifier(for: indexPath)
             }
-            guard let item = dataSource.itemIdentifier(for: indexPath) else {
-                return Observable.empty()
-            }
-            
-            return Observable.just(item)
-        }
         
         return ControlEvent(events: source)
     }
 }
 
 extension Reactive where Base: UICollectionView {
-    func sectionSelected<SectionIdentifierType, ItemIdentifierType>(_ dataSource: UICollectionViewDiffableDataSource<SectionIdentifierType, ItemIdentifierType>) -> ControlEvent<SectionIdentifierType> where SectionIdentifierType: Hashable, SectionIdentifierType: Sendable, ItemIdentifierType: Hashable, ItemIdentifierType: Sendable {
-        let source: Observable<SectionIdentifierType> = self.itemSelected.flatMap { [weak view = self.base as UICollectionView] indexPath -> Observable<SectionIdentifierType> in
-            guard view != nil else {
-                return Observable.empty()
+    func sectionSelected<SectionIdentifierType, ItemIdentifierType>(_ dataSource: UICollectionViewDiffableDataSource<SectionIdentifierType, ItemIdentifierType>) -> ControlEvent<SectionIdentifierType> where SectionIdentifierType: Hashable, ItemIdentifierType: Hashable {
+        let source: Observable<SectionIdentifierType> = self.itemSelected
+            .compactMap { [weak dataSource] indexPath in
+                dataSource?.sectionIdentifier(for: indexPath.section)
             }
-            guard let section = dataSource.sectionIdentifier(for: indexPath.section) else {
-                return Observable.empty()
-            }
-            
-            return Observable.just(section)
-        }
         
         return ControlEvent(events: source)
     }
     
-    func itemSelected<SectionIdentifierType, ItemIdentifierType>(_ dataSource: UICollectionViewDiffableDataSource<SectionIdentifierType, ItemIdentifierType>) -> ControlEvent<ItemIdentifierType> where SectionIdentifierType: Hashable, SectionIdentifierType: Sendable, ItemIdentifierType: Hashable, ItemIdentifierType: Sendable {
-        let source: Observable<ItemIdentifierType> = self.itemSelected.flatMap { [weak view = self.base as UICollectionView] indexPath -> Observable<ItemIdentifierType> in
-            guard view != nil else {
-                return Observable.empty()
+    func itemSelected<SectionIdentifierType, ItemIdentifierType>(_ dataSource: UICollectionViewDiffableDataSource<SectionIdentifierType, ItemIdentifierType>) -> ControlEvent<ItemIdentifierType> where SectionIdentifierType: Hashable, ItemIdentifierType: Hashable {
+        let source: Observable<ItemIdentifierType> = self.itemSelected
+            .compactMap { [weak dataSource] indexPath in
+                dataSource?.itemIdentifier(for: indexPath)
             }
-            guard let item = dataSource.itemIdentifier(for: indexPath) else {
-                return Observable.empty()
-            }
-            
-            return Observable.just(item)
-        }
         
         return ControlEvent(events: source)
     }
