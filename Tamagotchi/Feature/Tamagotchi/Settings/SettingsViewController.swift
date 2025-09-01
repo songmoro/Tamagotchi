555feb4f1469fd9429641d81ec590e83fe676935
songmoro <wlqgkr@naver.com>
2025-09-01 14:18:16 +0900

    디퍼블 소스 익스텐션 코드 분리

--- Tamagotchi/Feature/Tamagotchi/Settings/SettingsViewController.swift
+++ Tamagotchi/Feature/Tamagotchi/Settings/SettingsViewController.swift
@@ -10,38 +10,6 @@ import SnapKit
 import RxSwift
 import RxCocoa
 
-extension Reactive where Base: UITableView {
-    func sectionSelected<SectionIdentifierType, ItemIdentifierType>(_ dataSource: UITableViewDiffableDataSource<SectionIdentifierType, ItemIdentifierType>) -> ControlEvent<SectionIdentifierType> where SectionIdentifierType: Hashable, SectionIdentifierType: Sendable, ItemIdentifierType: Hashable, ItemIdentifierType: Sendable {
-        let source: Observable<SectionIdentifierType> = self.itemSelected.flatMap { [weak view = self.base as UITableView] indexPath -> Observable<SectionIdentifierType> in
-            guard view != nil else {
-                return Observable.empty()
-            }
-            guard let section = dataSource.sectionIdentifier(for: indexPath.section) else {
-                return Observable.empty()
-            }
-            
-            return Observable.just(section)
-        }
-        
-        return ControlEvent(events: source)
-    }
-    
-    func itemSelected<SectionIdentifierType, ItemIdentifierType>(_ dataSource: UITableViewDiffableDataSource<SectionIdentifierType, ItemIdentifierType>) -> ControlEvent<ItemIdentifierType> where SectionIdentifierType: Hashable, SectionIdentifierType: Sendable, ItemIdentifierType: Hashable, ItemIdentifierType: Sendable {
-        let source: Observable<ItemIdentifierType> = self.itemSelected.flatMap { [weak view = self.base as UITableView] indexPath -> Observable<ItemIdentifierType> in
-            guard view != nil else {
-                return Observable.empty()
-            }
-            guard let item = dataSource.itemIdentifier(for: indexPath) else {
-                return Observable.empty()
-            }
-            
-            return Observable.just(item)
-        }
-        
-        return ControlEvent(events: source)
-    }
-}
-
 final class SettingsViewController: ViewController<SettingsViewModel> {
     var delegate: SettingsViewControllerDelegate?
     
